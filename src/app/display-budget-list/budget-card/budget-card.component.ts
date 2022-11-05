070fc582541acbbd639f5fac1975c1323a103080
Shijo Shaji <jojovms@gmail.com>
2022-11-05 16:26:09 +0530

    UI enchancement

--- src/app/display-budget-list/budget-card/budget-card.component.ts
+++ src/app/display-budget-list/budget-card/budget-card.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-budget-card',
@@ -7,9 +7,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BudgetCardComponent implements OnInit {
 
+  @Input()
+  isIncome: boolean | undefined;
+
+  amount = 150;
+  currencyType = 'INR';
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  deleteBudget() {
+    console.log('deleted');
+
   }
 
 }
