e0bb9c9d749c0d0d92f2bf2e256b9eb62354db16
Shijo Shaji <jojovms@gmail.com>
2022-11-05 15:23:04 +0530

    component to display each item as cards

---
+++ src/app/display-budget-list/budget-card/budget-card.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-budget-card',
+  templateUrl: './budget-card.component.html',
+  styleUrls: ['./budget-card.component.scss']
+})
+export class BudgetCardComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
