2df9ea7d663a154a3e57a690f17f02b5a822f308
Shijo Shaji <jojovms@gmail.com>
2022-11-05 15:21:34 +0530

    setting skeleton

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { AddBudgetFormComponent } from './add-budget-form/add-budget-form.component';
+import { DisplayBudgetListComponent } from './display-budget-list/display-budget-list.component';
+import { BudgetCardComponent } from './display-budget-list/budget-card/budget-card.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    AddBudgetFormComponent,
+    DisplayBudgetListComponent,
+    BudgetCardComponent
   ],
   imports: [
     BrowserModule,
